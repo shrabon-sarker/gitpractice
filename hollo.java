c8ad8e2604f705049edfb7d68f6ede4565e2338c
shrabon-sarker <bikash.webdeveloper5@gmail.com>
2021-01-11 21:05:35 +0600

    this is  java new function adding

--- hollo.java
+++ hollo.java
@@ -1,5 +1,11 @@
 public class hello {
-    public static void main(strig[] args){
+    public static void main(String[] args){
         system.out.println("this is vs com")
+
+        frist("shrabon")
+    }
+
+    public static void frist(String[] name){
+        System.out.println('wellcime' + name)
     }
 }
